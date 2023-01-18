905da06d2978843db6211d0cd91b4fb342c140b9
s4iTen <ychouk102@gmail.com>
2023-01-18 12:06:55 +0100

    this function check the argument input and parse into a list of arguments and their index

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
 
     for i in range(line_len):
         if line_len == 1:
-            print("{}: arguments.".format(line_len - 1))
+            print("{} arguments.".format(line_len - 1))
+        elif line_len == 2:
+            if i == 1:
+                print("{} argument.".format(line_len - 1))
+                print("{}: {}".format(i, sys.argv[i]))
         elif i == 0:
             print("{} arguments:".format(line_len - 1))
         else:
