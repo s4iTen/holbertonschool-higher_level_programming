5c129a18d3d5e7491ce80d9f844323bdf38e5d63
s4iTen <ychouk102@gmail.com>
2023-01-24 12:24:03 +0100

    this function create a copy of the main list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pythin3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    list1 = [i for i in my_list]
+    list1[idx] = element
+    return list1
