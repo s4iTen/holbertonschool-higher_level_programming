da30852be7fbf4cd804efa154b3cfd154c94ea91
s4iTen <ychouk102@gmail.com>
2023-01-24 11:29:34 +0100

    this function change the number with index in my list with the new element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,4 +4,6 @@ def replace_in_list(my_list, idx, element):
         return None
     elif  idx > len(my_list) - 1:
         return my_list
+    else:
+        my_list[idx] = element
     return my_list
