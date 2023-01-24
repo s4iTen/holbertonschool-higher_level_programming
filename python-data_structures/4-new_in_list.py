e2ba36e402aeefaabd00726ab99af28f639eb313
s4iTen <ychouk102@gmail.com>
2023-01-24 12:32:19 +0100

    this function create a copy of the main list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    list1 = [i for i in my_list]
+    list1 = my_list[:]
     list1[idx] = element
     return list1
