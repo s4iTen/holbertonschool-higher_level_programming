0229889cbef7c0292455c0152340f99827702737
s4iTen <ychouk102@gmail.com>
2023-01-27 10:29:31 +0100

    this function print the number of elements of my list

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i, element in enumerate(my_list):
+        if i == x:
+            break
+        try:
+            print("{:d}".format(element), end='')
+            count += 1
+        except:
+            continue
+    print()
+    return count
