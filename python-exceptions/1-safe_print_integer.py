712d6c826d45b5223710de297979c7866a52d87c
s4iTen <ychouk102@gmail.com>
2023-01-27 10:02:25 +0100

    this function return true is the value is integer otherwise return false

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,4 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
-        print("{}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
