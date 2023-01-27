55cc51e31f72b53eefed5383d82e37b953c16e73
s4iTen <ychouk102@gmail.com>
2023-01-27 23:15:05 +0100

    this function print the list of integers safely

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
     for i, element in enumerate(my_list):
-        if i == x:
-            break
         try:
             print("{:d}".format(element), end='')
-            count += 1
-        except (TypeError, ValueError, IndexError):
-            continue
-    print()
-    return count
+        except Exception as e:
+            print(e)
