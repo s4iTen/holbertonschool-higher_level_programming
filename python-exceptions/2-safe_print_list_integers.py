a571032e9f59e695cedc476d2686792ff4221eea
s4iTen <ychouk102@gmail.com>
2023-01-27 10:39:15 +0100

    this function print the number of elements of my list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(element), end='')
             count += 1
-        except (TypeError, ValueError):
+        except IndexError as e:
+            print(e)
             continue
     print()
     return count
