410ddd1f1cd6476c75f0c03fd7de000593de2962
s4iTen <ychouk102@gmail.com>
2023-01-30 17:44:06 +0100

    this function print the square with adding an argument of the spaces and the break line

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,10 +4,13 @@
 
 class Square:
     """Represent Square"""
-    def __init__(self, size=0,tuple=(0, 0)):
+    def __init__(self, size=0,position=(0, 0)):
         self.__size = size
-        self.__tuple = tuple
+        self.__position = position
 
+    @property
+    def position(self):
+        return self.__position
     @property
     def size(self):
         return self.__size
@@ -25,12 +28,12 @@ class Square:
 
     def my_print(self):
         if self.size > 0:
-            if self.__tuple[1] > 0:
-                for i in range(self.__tuple[1]):
+            if self.__position[1] > 0:
+                for i in range(self.__position[1]):
                     print()
             for i in range(self.size):
-                if self.__tuple[0] > 0:
-                    for i in range(self.__tuple[0]):
+                if self.__position[0] > 0:
+                    for i in range(self.__position[0]):
                         print(" ", end="")
                 for i in range(self.size):
                     print("#", end="")
