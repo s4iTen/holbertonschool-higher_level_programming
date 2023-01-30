4cde69c3e965f8b2afda43fdfee8ee9923fcc258
s4iTen <ychouk102@gmail.com>
2023-01-30 16:07:24 +0100

    this function return the square area using the get and set

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a Class Square"""
+
+
+class Square:
+    """Represent Square"""
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
