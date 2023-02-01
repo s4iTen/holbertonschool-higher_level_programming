c8379f82a1d59328dcf68cda23d1199c65fbd3ba
s4iTen <ychouk102@gmail.com>
2023-02-01 13:39:45 +0100

    this function check the width and the height of the rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """this is the main class function"""
     def __init__(self, width, height):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     """This is the method width"""
     @property
@@ -15,11 +15,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        self.__width = value
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
@@ -27,8 +27,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        self.__height = value
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
