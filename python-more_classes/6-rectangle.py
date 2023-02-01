0f2f0bb679a0ac9e8fd6f8cadd440cae3c15beae
s4iTen <ychouk102@gmail.com>
2023-02-01 15:36:44 +0100

    this function create a counter of instance and it increasing and decreasing while we add or remode an instance

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""this is the documentathin of the class Rectangle"""
+
+
+class Rectangle:
+    number_of_instances = 0
+    """this is the main class function"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    """This is the method width"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if not self.__height or not self.__width:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        rec = ""
+        if self.__height and self.__width:
+            for i in range(self.__height):
+                if i < self.__height - 1:
+                    rec += "#" * self.__width + "\n"
+                else:
+                    rec += "#" * self.__width
+        return rec
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
