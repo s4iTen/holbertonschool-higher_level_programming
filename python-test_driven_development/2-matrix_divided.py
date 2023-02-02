3728277fb5001855fda00ca2b8825ee0dd39b2dd
s4iTen <ychouk102@gmail.com>
2023-02-03 00:37:31 +0100

    this function create a new matrix and divide it by an integer or float

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,8 +8,9 @@ def matrix_divided(matrix, div):
         if not isinstance([i for i in matrix[i]], list):
             if not isinstance([j for i in matrix[i][j]], int):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len([i for i in range(len(matrix))]) > len([i + 1 for i in range(len(matrix))]):
-        raise TypeError("Each row of the matrix must have the same size")
+    for i in range(len(matrix)):
+        if len(matrix[i]) != len(matrix[i + 1]):
+            raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
@@ -20,5 +21,4 @@ def matrix_divided(matrix, div):
     for i in range(len(new)):
         for j in range(len(new[i])):
             new[i][j] = round((new[i][j] / div), 2)
-
     return new
