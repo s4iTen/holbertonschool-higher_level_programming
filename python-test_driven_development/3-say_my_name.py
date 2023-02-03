91238bd3371222c962596681f7344b7499ff6c73
s4iTen <ychouk102@gmail.com>
2023-02-03 01:39:04 +0100

    updates

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""this function print the first and the last name"""
+
+
 def say_my_name(first_name, last_name=""):
-    """this function print the first and the last name"""
+    """and it raise an error if the type of the name is not a string """
+
 
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
