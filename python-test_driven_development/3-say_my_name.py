8f24f3e15ce422193d2691888d8fb9031e89700b
s4iTen <ychouk102@gmail.com>
2023-02-03 01:40:33 +0100

    updates

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,6 @@
 
 def say_my_name(first_name, last_name=""):
     """and it raise an error if the type of the name is not a string """
-
-
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
