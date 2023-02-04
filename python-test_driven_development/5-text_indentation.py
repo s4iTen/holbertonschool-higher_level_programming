73032eabb3c2373f428bb8987e392a8443184fcb
s4iTen <ychouk102@gmail.com>
2023-02-04 14:35:02 +0100

    this function print the text but every . or ? or : it make a blank line

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new = ""
+    for char in text:
+        new += char
+        if char in ['?', '.', ':']:
+            new += '\n\n'
+    print(new)
