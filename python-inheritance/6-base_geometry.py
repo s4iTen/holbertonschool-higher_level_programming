4209fd2deeb24a4a47b36abcd8f7c3a6d84e93cc
s4iTen <ychouk102@gmail.com>
2023-02-07 01:09:10 +0100

    this function just raise an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this is the class"""
+
+
+class BaseGeometry:
+    """this is the class content"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
