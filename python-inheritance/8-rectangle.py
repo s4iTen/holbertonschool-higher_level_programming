793644b9c69e98aa5209458604e1d7889f58d4e2
s4iTen <ychouk102@gmail.com>
2023-02-07 23:55:02 +0100

    this function check the height and the width and creat an inherit from another class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """this is the declaration of the class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle (BaseGeometry):
