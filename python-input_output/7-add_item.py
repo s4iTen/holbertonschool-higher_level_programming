f4305cec17df1ddf846d4b9ba4cd6773054c0a6f
Yahya_Chouk <ychouk102@gmail.com>
2023-02-09 00:22:23 +0100

    this function write the args in a JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,9 +5,12 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    """this cont is the list that we are going to load in the json file"""
-    cont = load_from_json_file('add_item.json')
-    for i in sys.argv[1:]:
-        cont.append(i)
-    save_to_json_file(cont, 'add_item.json')
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
