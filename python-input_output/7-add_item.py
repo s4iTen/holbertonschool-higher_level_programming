9018db646a9a982ecbcb0259c39b8287ba3df1c8
Yahya_Chouk <ychouk102@gmail.com>
2023-02-09 00:12:00 +0100

    this function get the args and write it in JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,9 +5,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """this is the declaration of the Module"""
 
-
 cont = load_from_json_file('add_item.json')
-save_to_json_file(cont, 'add_item.json')
 """
 this cont is the list that we are going to load in the json file
 """
