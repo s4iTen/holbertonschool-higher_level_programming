6358c47da200b6f10519cc3ac99a001941c35de5
Yahya_Chouk <ychouk102@gmail.com>
2023-02-08 18:39:26 +0100

    this function take the argv and write it in the JSON file as a list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""this function write the argv in JSON file"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""this function write the argv in JSON file"""
 
 
 cont = load_from_json_file('add_item.json')
