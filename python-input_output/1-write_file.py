8ca6676bf3d57aa78e256cc25f5761b76f0d99de
Yahya_Chouk <ychouk102@gmail.com>
2023-02-08 14:04:53 +0100

    this function over write in the file if it exist otherwise create a file and write in it

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""this is the declaration of the module"""
+
+
 def write_file(filename="", text=""):
     """this Module open the file and write in it"""
     with open(filename, 'w', encoding='utf8') as f:
