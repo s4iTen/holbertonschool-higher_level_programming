11fc3b389ae60ad2d7cc05ca431ee8a77e202f85
s4iTen <ychouk102@gmail.com>
2023-02-08 11:59:33 +0100

    this function check the size and return the area of the square and change the functionality of the print of the class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this is the declaration of the class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """this is the main method"""
+    def __init__(self, size):
+        self.__size = size
+        Rectangle.integer_validator(self, 'size', size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
