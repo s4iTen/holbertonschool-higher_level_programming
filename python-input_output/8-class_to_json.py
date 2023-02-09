49ff3fe632530894c5ad4fbac926a3d834cbd5f4
Yahya_Chouk <ychouk102@gmail.com>
2023-02-09 13:30:55 +0100

    this function return a dictionary like a representation of an object that can be used for JSON serialization

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,18 +6,4 @@ def class_to_json(obj):
     """
     this function return a dictionary like representation of the object
     """
-    if isinstance(obj, (int, float, str, bool)):
-        return obj
-    elif isinstance(obj, list):
-        for item in obj:
-            return class_to_json(item)
-    elif isinstance(obj, dict):
-        return {key: class_to_json(value) for key, value in obj.items()}
-    elif isinstance(obj, object):
-        obj_dict = {}
-        for attr, value in obj.__dict__.items():
-            obj_dict[attr] = class_to_json(value)
-        return obj_dict
-    else:
-        i = (type(obj).__name__)
-        raise TypeError(f"Object of type '{i}' is not JSON serializable")
+    return obj.__dict__
