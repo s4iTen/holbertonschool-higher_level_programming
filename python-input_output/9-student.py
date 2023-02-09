2e540eb749b631563ec615d0036ecde9af5b3c0e
Yahya_Chouk <ychouk102@gmail.com>
2023-02-09 13:47:39 +0100

    this function return a dictionary representation of an object

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,9 +5,9 @@
 class Student:
     """this is the main Module"""
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         return self.__dict__
