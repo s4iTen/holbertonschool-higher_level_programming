255c17e35521c6097e1f2abedfa06305534a2a28
Yahya_Chouk <ychouk102@gmail.com>
2023-02-09 12:46:45 +0100

    this function return a dictionary like a representation of an object that can be used for JSON serialization

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""this is the declaration of the module"""
+"""this is the declaration of the Module"""
 
 
 def class_to_json(obj):
-    """this function return a dictionary discription"""
-    obj_dict = {}
-    for attr, value in obj.__dict__.items():
-        if isinstance(value, (int, float, str, bool)):
-            obj_dict[attr] = value
-        elif isinstance(value, list):
-            obj_dict[attr] = [i for i in value]
-        elif isinstance(value, dict):
-            obj_dict[attr] = {key: value for key, value in value}
-    return obj_dict
-    
+    """
+    this function return a dictionary like representation of the object
+    """
+    if isinstance(obj, (int, float, str, bool)):
+        return obj
+    elif isinstance(obj, list):
+        for item in obj:
+            return class_to_json(item)
+    elif isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, object):
+        obj_dict = {}
+        for attr, value in obj.__dict__.items():
+            obj_dict[attr] = class_to_json(value)
+        return obj_dict
+    else:
+        i = (type(obj).__name__)
+        raise TypeError(f"Object of type '{i}' is not JSON serializable")
