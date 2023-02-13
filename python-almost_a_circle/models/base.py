a1a1ddd10ea5ba7125b3612ffcf1913d648c1085
s4iTen <ychouk102@gmail.com>
2023-02-13 16:17:52 +0100

    this function assign the id to the self id     or assign the public instance attribute

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,15 +2,15 @@
 """this is the declaration of the class base"""
 
 
-class base:
+class Base:
     """
     this function assign the id to the self id
     or assign the public instance attribute
     """
     __nb_objects = 0
     def __init__(self, id=None):
-        base.__nb_objects += 1
+        Base.__nb_objects += 1
         if id is None:
-            self.id =  base.__nb_objects
+            self.id =  Base.__nb_objects
         else:
             self.id = id
