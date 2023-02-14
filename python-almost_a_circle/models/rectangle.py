047ef240d7816fc51449ca33df87b2780f7fd6cc
s4iTen <ychouk102@gmail.com>
2023-02-14 22:43:48 +0100

    this function take the argument and assign it to the arguments of the main function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,16 @@ class Rectangle(Base):
         h = self.__height
         w = self.__width
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {w}/{h}")
+
+    def update(self, *args):
+        arg_count = len(args)
+        if arg_count > 0:
+            self.id = args[0]
+        if arg_count > 1:
+            self.width = args[1]
+        if arg_count > 2:
+            self.height = args[2]
+        if arg_count > 3:
+            self.x = args[3]
+        if arg_count > 4:
+            self.y = args[4]
