a1520e9bb274d09853cd3854e5ef5c5ad6c8b065
s4iTen <ychouk102@gmail.com>
2023-02-14 11:22:02 +0100

    this function assign the id to the self id or assign the public instance attribute

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,9 +8,10 @@ class Base:
     or assign the public instance attribute
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         Base.__nb_objects += 1
         if id is None:
-            self.id =  Base.__nb_objects
+            self.id = Base.__nb_objects
         else:
             self.id = id
