7ba867a32689f449835dae7171a7b00e38ef344e
s4iTen <ychouk102@gmail.com>
2023-02-14 13:42:46 +0100

    this function call the super class to interract with a variable

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,7 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -61,5 +61,5 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__y = value
+            raise ValueError("y must be >= 0")
+        self._y = value
