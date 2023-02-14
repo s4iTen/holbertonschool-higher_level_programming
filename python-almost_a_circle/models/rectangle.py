0590943d2f044db22b98befd8c48b85032131ed4
s4iTen <ychouk102@gmail.com>
2023-02-14 18:08:34 +0100

    this is the last updates of the display fnction

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,8 +70,10 @@ class Rectangle(Base):
 
     def display(self):
         """this function display the rectangle"""
+        for y in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         h = self.__height
