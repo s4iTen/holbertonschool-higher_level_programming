e4d17ff65820400a528a8ec26c7eb0eb766b079d
s4iTen <ychouk102@gmail.com>
2023-02-15 13:58:19 +0100

    this function return the json string representatio, of list_dictionarys

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """this is the declaration of the class base"""
+import json
 
 
 class Base:
@@ -15,3 +16,6 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
