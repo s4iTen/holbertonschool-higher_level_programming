847f382bd02f6bc41ab9b41e16c861a5df60773f
s4iTen <ychouk102@gmail.com>
2023-02-16 14:36:46 +0100

    this function writes a json representation of list_objs to a file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,16 @@ class Base:
         if list_dictionaries is None:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """this function writes a json representation
+         of list_objs to a file"""
+        print(cls.__name__)
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            dict_list = []
+            for obj in list_objs:
+                dict_list.append(obj.to_dictionary())
+            json_str = cls.to_json_string(dict_list)
+            file.write(json_str)
