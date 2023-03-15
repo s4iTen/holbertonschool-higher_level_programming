d9d353aea13f3d6da6d7bb63449f25831097c12c
s4iTen <ychouk102@gmail.com>
2023-03-15 14:15:13 +0100

    This script create a table and set its value

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- This script create a table and set its value
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table VALUES(1, "Jhon", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
