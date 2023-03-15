018502d4cad70698a2e4cc9a000667088f7f76f7
s4iTen <ychouk102@gmail.com>
2023-03-15 14:23:02 +0100

    This script create a table and set its value

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- This script create a table and set its value
-DROP TABLE second_table;
+DROP TABLE IF EXISTS second_table;
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
