4a8d17223ec239505ab5fe990402cf5094b37405
s4iTen <ychouk102@gmail.com>
2023-03-17 12:07:34 +0100

    This script list all the records and ordered by the id and sorted

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
--- This script list all the records that its name is California and ordered by the id and sorted
-SELECT * FROM hbtn_0d_usa.cities WHERE `name` = 'California'
-    ORDER BY state_id DESC;
+-- This script list all the records and ordered by the id and sorted
+SELECT * FROM hbtn_0d_usa.state_id
+    ORDER BY cities.id DESC;
