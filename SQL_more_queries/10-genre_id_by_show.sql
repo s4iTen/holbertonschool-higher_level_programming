2668ed39e7b23481ba187dec7d1befca99d88c00
s4iTen <ychouk102@gmail.com>
2023-03-20 13:30:35 +0100

    This script list all shows in hbtn_0d_tvshows databases

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- This script list all shows in hbtn_0d_tvshows databases
-SELECT s.title, gen.show_id FROM tv_shows AS s
+SELECT s.title, gen.genre_id FROM tv_shows AS s
  INNER JOIN tv_show_genres AS gen ON s.id = gen.show_id
- ORDER BY s.title ASC , gen.show_id ASC;
+ ORDER BY s.title ASC , gen.genre_id ASC;
