8a670986b04f66e5c5d7b751fa835869406ef74f
s4iTen <ychouk102@gmail.com>
2023-03-20 12:57:16 +0100

    This script list all shows in hbtn_0d_tvshows databases

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- This script list all shows in hbtn_0d_tvshows databases
+SELECT s.title, gen.genre_id FROM tv_shows AS s
+ INNER JOIN tv_show_genres AS gen ON s.id = gen.genre_id
+ ORDER BY s.title, gen.genre_id ASC;
