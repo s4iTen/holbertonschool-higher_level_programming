0a8e5e7499848eef2f7b62ff3bae774bbf9c905c
s4iTen <ychouk102@gmail.com>
2023-03-20 13:42:58 +0100

    this script list the tvshow name and the genre and if it null it just list it

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- This script list all shows in hbtn_0d_tvshows databases
+SELECT s.title, gen.genre_id FROM tv_shows AS s
+ LEFT JOIN tv_show_genres AS gen ON s.id = gen.show_id
+ ORDER BY s.title ASC , gen.genre_id ASC;
