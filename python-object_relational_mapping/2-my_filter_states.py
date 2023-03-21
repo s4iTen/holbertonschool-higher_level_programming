ce469ef247bdd2b3d27332aae412e0082f4d3d5b
s4iTen <ychouk102@gmail.com>
2023-03-21 11:59:48 +0100

    this function search a specific name in the table

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,15 +9,19 @@ if __name__ == '__main__':
         print(len(sys.argv))
         sys.exit(1)
 
-    h = "localhost"
-    u = sys.argv[1]
-    search = sys.argv[4]
-    conne = MySQLdb.connect(host=h, user=u, password='', db=sys.argv[3])
-    curs = conne.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = curs.fetchall()
-    for row in rows:
-        if row[1] == search:
+    try:
+        h = "localhost"
+        u = sys.argv[1]
+        search = sys.argv[4]
+        conne = MySQLdb.connect(host=h, user=u, password='', db=sys.argv[3])
+        curs = conne.cursor()
+        curs.execute("SELECT * \
+                      FROM states WHERE states.name\
+                      = '{}' ORDER BY id ASC".format(search))
+        rows = curs.fetchall()
+        for row in rows:
             print(row)
-    curs.close()
-    conne.close()
+        curs.close()
+        conne.close()
+    except Exception as e:
+        print(e)
