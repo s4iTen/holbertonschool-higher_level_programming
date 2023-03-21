325cdd050b90e54a6c12abada725e846badbb023
s4iTen <ychouk102@gmail.com>
2023-03-21 11:18:01 +0100

    this function take the argument and connect the host of sql

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print(len(sys.argv))
+        sys.exit(1)
+
+    conne = MySQLdb.connect(host = "localhost", user = sys.argv[1] ,password = '', db = sys.argv[3])
+    curs = conne.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    conne.close()
