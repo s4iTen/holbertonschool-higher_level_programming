4b167ce0824b40f8570682a153fc790e30fc787f
s4iTen <ychouk102@gmail.com>
2023-03-22 11:51:50 +0100

    this is the listing of the table ordered by the state id using SQLalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
-This script lists all the state's Objects from the database hbtn_0e_6_usa Ordered by state is
-and take the Username, the Password and the DB as an arguments.
+    This script lists all the state's Objects from the database hbtn_0e_6_usa
+    Ordered by state isand take the Username,
+    the Password and the DB as an arguments
 """
 
 
-# Importing the sql alchemy To interract with the DB
-# Impor the sys Module to use the argument
-# Import the sessionmaker to make a new session
-# Import the State Module to get the class Attributes
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
