f5df60db6745eb3a9f2fbe3ee3ca3ca60ecda1cc
s4iTen <ychouk102@gmail.com>
2023-04-19 22:25:26 +0200

    this is the last updates

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
 
-if (myVar == undefined) {
+if (process.argv[3]) {
+    console.log('Arguments found');
+}
+else if (myVar == undefined) {
   console.log('No argument');
 } else { console.log('Argument found'); }
