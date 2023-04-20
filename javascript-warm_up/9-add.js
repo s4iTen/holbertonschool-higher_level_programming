fba7bef774a4bc06edc8ee3cab9373600a57b722
s4iTen <ychouk102@gmail.com>
2023-04-20 11:17:34 +0200

    this function is an addition function

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+    return parseInt(a) + parseInt(b);
+  }
+  const num1 = process.argv[2];
+  const num2 = process.argv[3];
+  console.log(add(num1, num2));
+  
