fddebdf86739d6285f85bb4cac659c4445a4edfa
s4iTen <ychouk102@gmail.com>
2023-04-20 11:37:10 +0200

    this is the last updates

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-    return parseInt(a) + parseInt(b);
-  }
-  const num1 = process.argv[2];
-  const num2 = process.argv[3];
-  console.log(add(num1, num2));
-  
+  return parseInt(a) + parseInt(b);
+}
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+console.log(add(num1, num2));
