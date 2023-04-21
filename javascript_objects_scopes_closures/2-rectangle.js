1c643a8f87ed9597b337d2e4d6b66eddb3c6087c
s4iTen <ychouk102@gmail.com>
2023-04-21 13:40:10 +0200

    this is the last updates

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
     constructor (w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
+      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+        return 'Rectangle{}';
       }
       this.width = w;
       this.height = h;
