feacd00d4c61d96d384fe826c7eefe60a972835e
s4iTen <ychouk102@gmail.com>
2023-04-25 13:41:49 +0200

    this function count the completed tasks for each user

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const completedTasksByUser = {};
-
-    data.forEach(function (task) {
-      if (task.completed) {
-        if (completedTasksByUser[task.userId]) {
-          completedTasksByUser[task.userId]++;
-        } else {
-          completedTasksByUser[task.userId] = 1;
-        }
-      }
-    });
-
-    console.log(completedTasksByUser);
-  } else {
-    console.error(`Error fetching data: ${error}`);
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
   }
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = {};
+  tasks.forEach(function (task) {
+    if (task.completed) {
+      if (!completedTasksByUser.hasOwnProperty(task.userId)) {
+        completedTasksByUser[task.userId] = 0;
+      }
+      completedTasksByUser[task.userId]++;
+    }
+  });
+  console.log(completedTasksByUser);
 });
